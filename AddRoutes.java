5b610db92c22dcfdfd52e11a8f77b6a86d5cfaf2
grabdoc <grabdoc2020@gmail.com>
2022-12-11 04:11:10 -0600

    adding add routes

--- AddRoutes.java
+++ AddRoutes.java
@@ -7,17 +7,7 @@ import java.util.concurrent.Callable;
         description = "Add Routes to a gateway using CLI.")
 public class AddRoutes implements Callable<Integer> {
 
-    @Option(names = { "--gatewayKey" } , description = "Gateway API Key", required = true)
-    private String gatewayKey;
 
-    @Option(names = { "--orgId" } , description = "Organization Id", required = true)
-    private String orgId;
-
-    @Option(names = { "--routeFile" } , description = "Route File", required = true)
-    private String routeFile;
-
-    @Option(names = { "--platformUrl" } , description = "Platform URL", required = true)
-    private String platformUrl;
 
     @Override
     public Integer call() throws Exception {
