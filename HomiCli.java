bb3068cad45235aa21a7633827d7af265b288c29
grabdoc <grabdoc2020@gmail.com>
2022-12-23 10:27:48 -0600

    minor improvements

--- HomiCli.java
+++ HomiCli.java
@@ -8,8 +8,8 @@
 //DEPS org.postgresql:postgresql:42.5.1
 //DEPS net.lingala.zip4j:zip4j:2.11.2
 //SOURCES GenerateRest.java
-//SOURCES Db.java
-//SOURCES Generator.java
+//SOURCES DbConfig.java
+//SOURCES GeneratorConfig.java
 //SOURCES JpaGenerator.java
 
 
