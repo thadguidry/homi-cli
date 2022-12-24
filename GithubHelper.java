7c67a0e6b14b0a340157f41902db08980f5eedb1
grabdoc <grabdoc2020@gmail.com>
2022-12-24 00:31:49 -0600

    incremental improvement

--- GithubHelper.java
+++ GithubHelper.java
@@ -3,7 +3,9 @@ import java.io.File;
 public class GithubHelper {
 
     public void init(String folder) {
-        File file = new File(folder + "/.github/workflows");
+        String gitFolder = folder + "/.github/workflows";
+        System.out.println("gitFolder : " + gitFolder);
+        File file = new File(gitFolder);
         file.mkdirs();
     }
 }
